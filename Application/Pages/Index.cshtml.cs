073d35de8d29f651ece9d6ae3eca832378df6659
Saeid Izadi <saeed.izadi8401@gmail.com>
2024-11-16 00:27:30 +0330

    add export py script and sample

--- Application/Pages/Index.cshtml.cs
+++ Application/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
 
 namespace Application.Pages
 {
@@ -14,7 +15,41 @@ namespace Application.Pages
 
         public void OnGet()
         {
+            // place python script path
+            var pyFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "py", "exportpdf.py");
 
+            // place your html here
+            string htmlContent = "<!DOCTYPE html>\r\n<html dir='rtl'>\r\n<head>\r\n <title>PDF Example</title>\r\n</head>\r\n<body> " +
+                                 "<h1>سلام این برنامه به صورت یک نمونه برای شما ساخته شده ! !</h1>\r\n</body>\r\n</html>";
+
+            // place your target pdf path and name here
+            var pdfOutPutPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "pdf", "pypdf.pdf");
+
+            // place your wkhtmltopdf path on your system
+            var wkhtmltopdfPath = "C://Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "python",
+                Arguments = $"\"{pyFilePath}\" \"{htmlContent}\" \"{pdfOutPutPath}\" \"{wkhtmltopdfPath}\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+                Console.WriteLine("Output: " + output);
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Console.WriteLine("Error: " + error);
+                }
+            }
         }
     }
 }
---
+++ Application/wwwroot/py/exportpdf.py
@@ -0,0 +1,12 @@
+﻿import sys
+import pdfkit
+
+options = {
+    'encoding': 'UTF-8',
+}
+
+html_file = sys.argv[1]
+pdf_file = sys.argv[2]
+config_path = sys.argv[3]
+config = pdfkit.configuration(wkhtmltopdf=config_path)
+pdfkit.from_string(html_file, pdf_file,options=options, configuration=config)
